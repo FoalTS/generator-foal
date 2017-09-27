a67f1b015c8e93b98600d643bc47e1852b1a524d
Loïc Poullain <loic.poullain@centraliens.net>
2017-09-27 21:22:41 +0200

    Change naming convention for controller files

--- generators/controller/index.js
+++ generators/controller/index.js
@@ -29,7 +29,7 @@ module.exports = class extends Generator {
     }
     this.fs.copyTpl(
       this.templatePath(`${this.options.type}-controller.ts`),
-      this.destinationPath(`${this.names.kebabName}.controller.ts`),
+      this.destinationPath(`${this.names.kebabName}-controller.service.ts`),
       this.names
     );
   }
