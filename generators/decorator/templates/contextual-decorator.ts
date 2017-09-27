065d2b883b3ef5bb1eb87997b508f714e27b0d79
Loïc Poullain <loic.poullain@centraliens.net>
2017-09-27 21:19:44 +0200

    Remove useless import in template

--- generators/decorator/templates/contextual-decorator.ts
+++ generators/decorator/templates/contextual-decorator.ts
@@ -1,5 +1,3 @@
-import { Request, Response, NextFunction } from 'express';
-
 import { Context, newContextualDecorator } from '@foal/core';
 
 export async function <%= camelName %>Middleware(ctx: Context): Promise<Context> {
