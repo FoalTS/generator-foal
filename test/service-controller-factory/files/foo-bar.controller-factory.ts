3e779cc9b6bc5c2547ef7ae81b0929015fa34247
Loïc Poullain <loic.poullain@centraliens.net>
2018-03-09 10:56:06 +0100

    Fix linting.

--- test/service-controller-factory/files/foo-bar.controller-factory.ts
+++ test/service-controller-factory/files/foo-bar.controller-factory.ts
@@ -1,4 +1,4 @@
-import { Controller, Class, HttpReponseOK, ServiceControllerFactory } from '@foal/core';
+import { Class, Controller, HttpReponseOK, ServiceControllerFactory } from '@foal/core';
 
 import { IFooBar } from './foo-bar.interface';
 
