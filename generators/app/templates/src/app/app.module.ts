3cdf9c7cc5938f2d3e19ebc2d86b140d0bddf463
Loïc Poullain <loic.poullain@centraliens.net>
2018-05-30 12:10:07 +0200

    Remove index service, use new controller-factory and update packages.

--- generators/app/templates/src/app/app.module.ts
+++ generators/app/templates/src/app/app.module.ts
@@ -1,15 +1,11 @@
-import { view } from '@foal/common';
-import { Module } from '@foal/core';
+import { Module, view } from '@foal/core';
 
 import { config } from '../config';<% if (authentication) { %>
 import { AuthModule } from './auth';<% } %>
-import { IndexViewService } from './index-view.service';
 
 export const AppModule: Module = {
   controllers: [
-    view
-      .attachService('/', IndexViewService)
-      .withPreHook(ctx => { ctx.state.locals = { appName: config.app.name }; })
+    view('/', require('./templates/index.html'), { appName: config.app.name } )
   ],<% if (authentication) { %>
   modules: [
     AuthModule
