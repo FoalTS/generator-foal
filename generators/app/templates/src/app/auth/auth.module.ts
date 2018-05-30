04030297a8ecd969ed62396470aaec1d4dc092b0
Loïc Poullain <loic.poullain@centraliens.net>
2018-05-30 11:49:11 +0200

    Use new controller factories.

--- generators/app/templates/src/app/auth/auth.module.ts
+++ generators/app/templates/src/app/auth/auth.module.ts
@@ -1,28 +1,18 @@
-import { authentication, validateEmailAndPasswordCredentialsFormat } from '@foal/authentication';
-import { view } from '@foal/common';
-import { Module } from '@foal/core';
+import { login, logout, Module, render, route, view } from '@foal/core';
 
-import { AuthenticatorService } from './authenticator.service';
-import { LoginViewService } from './login-view.service';
+import { Authenticator } from './authenticator.service';
 
 export const AuthModule: Module = {
   controllers: [
-    authentication
-      .attachService('/email', AuthenticatorService, {
-        failureRedirect: '/auth?invalid_credentials=true', // Optional
-        successRedirect: '/whatever_you_like', // Optional
-      })
-      .withPreHook(validateEmailAndPasswordCredentialsFormat()),
-    authentication
-      .attachLogout('/logout', { redirect: '/auth' }),
-    view
-      .attachService('/', LoginViewService)
-      .withPreHook(ctx => {
-        ctx.state.locals = {
-          csrfToken: ctx.state.csrfToken,
-          invalidCredentials: ctx.query.invalid_credentials
-        };
-      })
+    login('/email', Authenticator, {
+      failureRedirect: '/auth?invalid_credentials=true', // Optional
+      successRedirect: '/whatever_you_like', // Optional
+    }),
+    logout('/logout', { redirect: '/auth' }),
+    route('/', 'GET', ctx => render({
+      csrfToken: ctx.state.csrfToken,
+      invalidCredentials: ctx.query.invalid_credentials
+    }))
   ],
   path: '/auth',
 };
