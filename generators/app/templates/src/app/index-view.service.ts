f6877f77bd54429ef06a83ad13353ae2327c1a06
Loïc Poullain <loic.poullain@centraliens.net>
2018-05-30 11:47:37 +0200

    Remove view services.

--- generators/app/templates/src/app/index-view.service.ts
+++
@@ -1,9 +0,0 @@
-import { Service } from '@foal/core';
-import { EjsTemplateService } from '@foal/ejs';
-
-@Service()
-export class IndexViewService extends EjsTemplateService {
-  constructor() {
-    super(__dirname + '/templates/index.html');
-  }
-}
