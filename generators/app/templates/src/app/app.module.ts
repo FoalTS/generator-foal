18990be902faeb7adf7bb9ad7b3faa2dc84e718c
Loïc Poullain <loic.poullain@centraliens.net>
2018-06-07 14:36:51 +0200

    Use Config.

--- generators/app/templates/src/app/app.module.ts
+++ generators/app/templates/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { getConfig, Module, view } from '@foal/core';
+import { Config, Module, view } from '@foal/core';
 
 import { index } from './templates';
 
 export const AppModule: Module = {
   controllers: [
-    view('/', index, { appName: getConfig('base').app.name } )
+    view('/', index, { appName: Config.get('app', 'name') } )
   ],
 };
